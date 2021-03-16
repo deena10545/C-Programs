d4c8c400569fcf11aca3d0315de00828c5e6bfe6
Deena Dayalan M <76902858+deena10545@users.noreply.github.com>
2021-03-16 15:38:37 +0530

    Update 36.calculate lcm&hcf.c

--- 36.calculate lcm&hcf.c
+++ 36.calculate lcm&hcf.c
@@ -6,7 +6,7 @@ int main()
    int num1,num2,i,gcd,lcm;
    printf("Enter two numbers to find HCF and LCM:");
    scanf("%d %d",&num1,&num2);
-   for(i=1;i<num1&&i<num2;i++)
+   for(i=1;i<=num1&&i<=num2;i++)
    {
        if(num1%i==0&&num2%i==0)
             gcd=i;
